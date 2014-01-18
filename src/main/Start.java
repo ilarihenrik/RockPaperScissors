6835eade261957b47ef2c54c82f9fbd1c971fb65
ilarihenrik <myself@ilari.com>
2014-01-18 20:23:35 +0100

    test 2

--- src/main/Start.java
+++ src/main/Start.java
@@ -4,4 +4,11 @@ package main;
  * Created by ilari on 18.01.14.
  */
 public class Start {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
 }
+
+
